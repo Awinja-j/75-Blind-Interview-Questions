2bc459baecff3d5e4fff148c44ac9b93ca2fba7a
Awinja-j <ingari61@hotmail.com>
2022-03-27 18:48:13 +0300

    add code

---
+++ Matrix/200. Number of Islands.py
@@ -0,0 +1,63 @@
+'''
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+ 
+
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
+'''
+
+from typing import List
+
+def dfs(grid, visited, i, j):
+    if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0 or (i, j) in visited:
+        return
+    visited.append((i, j))
+    dfs(grid, visited, i+1, j)
+    dfs(grid, visited, i-1, j)
+    dfs(grid, visited, i, j+1)
+    dfs(grid, visited, i, j-1)
+
+def numIslands(grid: List[List[str]]) -> int:
+
+    if not grid:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    visited = []
+    islands = 0
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                islands += 1
+                dfs(grid, visited, i, j)
+
+    return islands
+
+print(numIslands([[1,1,0,0,0],[1,1,1,1,0],[1,0,0,0,0],[1,1,0,0,0],[1,1,1,1,1]]))
